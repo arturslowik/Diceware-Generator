2b92dad1047f14e6ba086afb0b84c0413e53375a
arturslowik <artur@slowik.it>
2017-06-19 11:44:33 +0200

    Reddit optimalisation part 1

--- diceware.py
+++ diceware.py
@@ -5,13 +5,13 @@
 
 from random import randint
 def diceRoll():
-#Array to store key for Diceware dictionary
-    key = []
-#Random loop to generate key. Five "dice" rolls to obtain one key
-    for i in range(0, 5):
-        i = randint(1, 6)
-        key.append(str(i))
-#String function removes all formating and stores key in the form 12345
+    #Array to store key for Diceware dictionary
+    key = [str(randint(1, 6)) for i in range(0, 5)]
+    #Random loop to generate key. Five "dice" rolls to obtain one key
+    #for i in range(0, 5):
+    #    i = randint(1, 6)
+    #    key.append(str(i))
+    #String function removes all formating and stores key in the form 12345
     return str((''.join(map(str, key))))
 
 #This part opens dictionary file (diceware.txt) and loops through it to look for key and its value
