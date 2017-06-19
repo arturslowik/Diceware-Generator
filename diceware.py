4056cd833eac9e687b10dde15b26e254d14dd113
arturslowik <artur@slowik.it>
2017-06-19 11:24:45 +0200

    Added main code

---
+++ diceware.py
@@ -0,0 +1,22 @@
+"""Diceware generator v0.1"""
+# Code written by Artur S.
+#
+#
+
+from random import randint
+def diceRoll():
+#Array to store key for Diceware dictionary
+    key = []
+#Random loop to generate key. Five "dice" rolls to obtain one key
+    for i in range(0, 5):
+        i = randint(1, 6)
+        key.append(str(i))
+#String function removes all formating and stores key in the form 12345
+    return str((''.join(map(str, key))))
+
+#This part opens dictionary file (diceware.txt) and loops through it to look for key and its value
+data = diceRoll()
+file = open("diceware.txt", "r")
+for line in file:
+    if data in line:
+        print(line)
