26d281a87e96ce9ce29edc5b121a002ee1e7e1a4
arturslowik <artur@slowik.it>
2017-06-20 06:33:03 +0200

    Minor fixes

--- diceware.py
+++ diceware.py
@@ -1,19 +1,25 @@
-"""Diceware generator v0.1"""
+"""Diceware generator"""
 # Code written by Artur S.
 #
 #
 from random import randint
+#Array word stores randomly generated number.
+#For loop ensures it is done 5 times to create one word.
+#Diceware definition says that one word is created by 5 throws.
 def diceRoll():
-    #Array word stores randomly generated number. For loop ensures it is done 5 times to create one word.
-    #Diceware definition says that one word is created by 5 results.
     word = [str(randint(1, 6)) for i in range(0, 5)]
 
     #String function removes all formating and stores word in the form 12345 required by Diceware
     return str((''.join(map(str, word))))
 
 #This part gets reult of diceRoll() as variable, opens dictionary file and loops through it.
-words = diceRoll()
-with open("diceware.txt") as file:
-    for line in file:
-        if words in line:
-            print(line)
+times = 6
+kek = []
+for i in range(times):
+    words = diceRoll()
+    with open("diceware.txt") as file:
+        for line in file:
+            if words in line:
+                kek.append(line)
+print(str((''.join(map(str, kek)))).rstrip())
+                #print(line.rstrip())
